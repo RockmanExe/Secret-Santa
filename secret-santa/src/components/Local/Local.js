fb7a3d6fce55ea7c002e6b7cef59ae9e2fb64fa6
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-10 18:32:57 -0700

    smth

---
+++ secret-santa/src/components/Local/Local.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
+import { Button } from 'reactstrap';
+
+class Local extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                Local
+
+            </div>
+        );
+    }
+}
+
+export default Local;
