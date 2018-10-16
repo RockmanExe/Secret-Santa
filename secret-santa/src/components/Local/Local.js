f5a38ff43547e953ccfbb680083f8fcb6d88b661
unknown <may.alex43@gmail.com>
2018-10-15 23:46:59 -0400

    added backend file, added knex, sqlite3 etc

--- secret-santa/src/components/Local/Local.js
+++ secret-santa/src/components/Local/Local.js
@@ -18,6 +18,15 @@ class Local extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+        const userInfo = { name: this.state.name }
+        console.log(userInfo);
+        axios
+            .post('smth/api/user', { name: this.state.name, wishes: this.state.wishes})
+        
+    } 
+
     render() {
         return (
             <div className='local-page'>
@@ -29,6 +38,7 @@ class Local extends Component {
                         value = {this.state.name}
                         onChange={this.handleChange}
 
+
                     />
                     <br/>
                     <Input
@@ -37,9 +47,10 @@ class Local extends Component {
                         value = {this.state.wish}
                         onChange={this.handleChange}
                     />
+                    <Button className='addWish' onClick={this.handleAdd}>+ Add</Button>
                 </Form>
                 
-                <Button className='member'>Submit Member</Button>
+                <Button className='member' onClick={this.handleSubmit}>Submit Member</Button>
                 <Link to='/api/local/pullingnames'><Button className='pull-names'>Start Pulling Names</Button></Link>
             </div>
         );
